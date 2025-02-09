5656a01634d000cf0b8f9d844741a198f740934b
pc <rockpiyush16640@gmail.com>
2025-02-09 14:57:05 +0530

    "added user service"

---
+++ Order-Processing-in-Real-time/src/main/java/com/order/process/service/UserService.java
@@ -0,0 +1,4 @@
+package com.order.process.service;
+
+public interface UserService {
+}
---
+++ Order-Processing-in-Real-time/src/main/java/com/order/process/service/impl/UserServiceImpl.java
@@ -0,0 +1,10 @@
+package com.order.process.service.impl;
+
+import com.order.process.service.UserService;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+
+@Service
+@Slf4j
+public class UserServiceImpl implements UserService {
+}
