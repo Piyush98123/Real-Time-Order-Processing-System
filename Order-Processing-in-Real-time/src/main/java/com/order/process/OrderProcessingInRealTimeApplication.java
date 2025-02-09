dcfb75e9102c47caab0ca75f05e0e5a1366a55a1
Piyush Kumar Singh <64677706+Piyush98123@users.noreply.github.com>
2025-02-09 13:50:30 +0530

    initial commit

---
+++ Order-Processing-in-Real-time/src/main/java/com/order/process/OrderProcessingInRealTimeApplication.java
@@ -0,0 +1,13 @@
+package com.order.process;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OrderProcessingInRealTimeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OrderProcessingInRealTimeApplication.class, args);
+	}
+
+}
